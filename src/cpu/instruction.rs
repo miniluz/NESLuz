4919951eacd5a240750c3f3182c0e63672a952f5
miniluz <javiermelon4fu@gmail.com>
2023-05-18 00:19:35 +0200

    Turned register into enum

--- src/cpu/instruction.rs
+++ src/cpu/instruction.rs
@@ -1,27 +1,35 @@
-use super::{memory::Memory, Cpu, RegisterImmutable, RegisterMutable};
+use super::{memory::Memory, Register};
 
 pub const LDA_IMMEDIATE: u8 = 0xa9;
+pub const LDA_ZERO_PAGE: u8 = 0xa5;
+pub const LDA_ZERO_PAGE_X: u8 = 0xb5;
+pub const LDA_ABSOLUTE: u8 = 0xad;
 pub const LDX_IMMEDIATE: u8 = 0xa2;
+pub const LDX_ZERO_PAGE: u8 = 0xa6;
 pub const TAX: u8 = 0xaa;
 pub const INX: u8 = 0xe8;
 
 #[derive(Debug)]
 pub enum LdType {
     Immediate { immediate: u8 },
+    ZeroPage { address: u8 },
+    ZeroPageX { address: u8 },
+    Absolute { address: u16 },
 }
 
+#[derive(Debug)]
 pub enum Instruction {
     Break,
     Ld {
-        destination: RegisterMutable,
+        destination: Register,
         ld_type: LdType,
     },
     Trr {
-        origin: RegisterImmutable,
-        destination: RegisterMutable,
+        origin: Register,
+        destination: Register,
     },
     In {
-        destination: RegisterMutable,
+        destination: Register,
     },
 }
 
@@ -32,47 +40,63 @@ fn null_pointer() -> color_eyre::Report {
 impl Instruction {
     pub fn get_instruction(
         memory: &Memory,
-        offset: &u16,
+        program_counter: &u16,
     ) -> color_eyre::Result<(Instruction, u16)> {
-        let mut instructions = memory.iter().skip(*offset as usize).enumerate();
-        let (offset, instruction) = match instructions.next().ok_or(null_pointer())? {
-            (_, 0x00) => (0, Instruction::Break),
-            (_, &LDX_IMMEDIATE) => {
-                // LDX Immidiate
-                let (offset, &immediate) = instructions.next().ok_or(null_pointer())?;
-                (
-                    offset,
-                    Instruction::Ld {
-                        destination: Cpu::register_x_mut,
-                        ld_type: LdType::Immediate { immediate },
-                    },
-                )
+        let mut program_counter = *program_counter;
+        let instruction = memory.read(program_counter)?;
+        program_counter += 1;
+        let instruction = match instruction {
+            0x00 => Instruction::Break,
+            LDA_IMMEDIATE | LDX_IMMEDIATE => {
+                let immediate = memory.read(program_counter)?;
+                program_counter += 1;
+                let destination = match instruction {
+                    LDA_IMMEDIATE => Register::A,
+                    LDX_IMMEDIATE => Register::X,
+                    _ => panic!("Code should be unreachable."),
+                };
+                Instruction::Ld {
+                    destination,
+                    ld_type: LdType::Immediate { immediate },
+                }
             }
-            (_, &LDA_IMMEDIATE) => {
-                let (offset, &immediate) = instructions.next().ok_or(null_pointer())?;
-                (
-                    offset,
-                    Instruction::Ld {
-                        destination: Cpu::register_a_mut,
-                        ld_type: LdType::Immediate { immediate },
-                    },
-                )
+            LDA_ZERO_PAGE | LDX_ZERO_PAGE => {
+                let address = memory.read(program_counter)?;
+                program_counter += 1;
+                let destination = match instruction {
+                    LDA_ZERO_PAGE => Register::A,
+                    LDX_ZERO_PAGE => Register::X,
+                    _ => panic!("Code should be unreachable."),
+                };
+                Instruction::Ld {
+                    destination,
+                    ld_type: LdType::ZeroPage { address },
+                }
             }
-            (_, &TAX) => (
-                // TAX
-                0,
-                Instruction::Trr {
-                    origin: Cpu::register_a,
-                    destination: Cpu::register_x_mut,
-                },
-            ),
-            (_, &INX) => (
-                // INX
-                0,
-                Instruction::In {
-                    destination: Cpu::register_x_mut,
-                },
-            ),
+            LDA_ZERO_PAGE_X => {
+                let address = memory.read(program_counter)?;
+                program_counter += 1;
+                Instruction::Ld {
+                    destination: Register::A,
+                    ld_type: LdType::ZeroPageX { address },
+                }
+            }
+            LDA_ABSOLUTE => {
+                let address = memory.read_u16(program_counter)?;
+                program_counter += 2;
+                Instruction::Ld {
+                    destination: Register::A,
+                    ld_type: LdType::Absolute { address },
+                }
+            }
+            TAX => Instruction::Trr {
+                origin: Register::A,
+                destination: Register::X,
+            },
+
+            INX => Instruction::In {
+                destination: Register::X,
+            },
             _ => {
                 return Err(color_eyre::eyre::eyre!(
                     "Should have been able to find instruction!"
@@ -80,6 +104,6 @@ impl Instruction {
             }
         };
 
-        Ok((instruction, u16::try_from(offset)? + 1))
+        Ok((instruction, program_counter))
     }
 }
--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -8,58 +8,125 @@ mod test {
 
     fn get_instruction(instructions: &[u8]) -> color_eyre::Result<(Instruction, u16)> {
         let mut memory = Memory::new();
-        memory.load(0x8000, instructions);
+        memory.load(0x8000, instructions)?;
         Instruction::get_instruction(&memory, &0x8000)
     }
 
     #[test]
-    fn lda() {
+    fn lda_immediate() {
         assert!(matches!(
             get_instruction(&[LDA_IMMEDIATE, 0xc0]).unwrap(),
             (
                 Instruction::Ld {
-                    destination: _,
+                    destination: Register::A,
                     ld_type: LdType::Immediate { immediate: 0xc0 }
                 },
-                2
+                0x8002,
             )
         ));
 
         let mut cpu = Cpu::new();
-        cpu.load_and_run(vec![LDA_IMMEDIATE, 0xf0, 0x00]).unwrap();
+        cpu.load_and_run_test(vec![LDA_IMMEDIATE, 0xf0, 0x00])
+            .unwrap();
         assert_eq!(cpu.register_a, 0xf0);
         assert!(cpu.status.get(Flags::Negative));
         assert!(!cpu.status.get(Flags::Zero));
 
         let mut cpu = Cpu::new();
-        cpu.load_and_run(vec![LDA_IMMEDIATE, 0x00, 0x00]).unwrap();
+        cpu.load_and_run_test(vec![LDA_IMMEDIATE, 0x00, 0x00])
+            .unwrap();
         assert_eq!(cpu.register_a, 0x00);
         assert!(!cpu.status.get(Flags::Negative));
         assert!(cpu.status.get(Flags::Zero));
     }
 
     #[test]
-    fn ldx() {
+    fn ldx_immediate() {
         assert!(matches!(
             get_instruction(&[LDX_IMMEDIATE, 0xc0]).unwrap(),
             (
                 Instruction::Ld {
-                    destination: _,
+                    destination: Register::X,
                     ld_type: LdType::Immediate { immediate: 0xc0 }
                 },
-                2
+                0x8002
             )
         ));
 
         let mut cpu = Cpu::new();
-        cpu.load_and_run(vec![LDX_IMMEDIATE, 0xf0, 0x00]).unwrap();
+        cpu.load_and_run_test(vec![LDX_IMMEDIATE, 0xf0, 0x00])
+            .unwrap();
         assert_eq!(cpu.register_x, 0xf0);
         assert!(cpu.status.get(Flags::Negative));
         assert!(!cpu.status.get(Flags::Zero));
 
         let mut cpu = Cpu::new();
-        cpu.load_and_run(vec![LDX_IMMEDIATE, 0x00, 0x00]).unwrap();
+        cpu.load_and_run_test(vec![LDX_IMMEDIATE, 0x00, 0x00])
+            .unwrap();
         assert_eq!(cpu.register_x, 0x00);
+        assert!(!cpu.status.get(Flags::Negative));
+        assert!(cpu.status.get(Flags::Zero));
+    }
+
+    #[test]
+    fn lda_zero_page() {
+        assert!(matches!(
+            get_instruction(&[LDA_ZERO_PAGE, 0xc0]).unwrap(),
+            (
+                Instruction::Ld {
+                    destination: Register::A,
+                    ld_type: LdType::ZeroPage { address: 0xc0 }
+                },
+                0x8002,
+            )
+        ));
+
+        let mut cpu = Cpu::new();
+        cpu.load(vec![LDA_ZERO_PAGE, 0x02, 0x00]).unwrap();
+        cpu.reset().unwrap();
+        cpu.program_counter = 0x8000;
+        cpu.memory.load(0x00, &[0x01, 0x02, 0xf1, 0x04]).unwrap();
+        cpu.run().unwrap();
+        assert_eq!(cpu.register_a, 0xf1);
+        assert!(cpu.status.get(Flags::Negative));
+        assert!(!cpu.status.get(Flags::Zero));
+
+        let mut cpu = Cpu::new();
+        cpu.load_and_run_test(vec![LDA_ZERO_PAGE, 0x02, 0x00])
+            .unwrap();
+        assert_eq!(cpu.register_a, 0x00);
+        assert!(!cpu.status.get(Flags::Negative));
+        assert!(cpu.status.get(Flags::Zero));
+    }
+
+    #[test]
+    fn lda_zero_page_x() {
+        assert!(matches!(
+            get_instruction(&[LDA_ZERO_PAGE_X, 0xc0]).unwrap(),
+            (
+                Instruction::Ld {
+                    destination: Register::A,
+                    ld_type: LdType::ZeroPageX { address: 0xc0 }
+                },
+                0x8002,
+            )
+        ));
+
+        let mut cpu = Cpu::new();
+        cpu.load(vec![LDA_ZERO_PAGE_X, 0x02, 0x00]).unwrap();
+        cpu.reset().unwrap();
+        cpu.program_counter = 0x8000;
+        cpu.register_x = 0x01;
+        cpu.memory.load(0x00, &[0x01, 0x02, 0x03, 0xf4]).unwrap();
+        cpu.run().unwrap();
+        assert_eq!(cpu.register_a, 0xf4);
+        assert!(cpu.status.get(Flags::Negative));
+        assert!(!cpu.status.get(Flags::Zero));
+
+        let mut cpu = Cpu::new();
+        cpu.load_and_run_test(vec![LDA_ZERO_PAGE_X, 0x02, 0x00])
+            .unwrap();
+        assert_eq!(cpu.register_a, 0x00);
         assert!(!cpu.status.get(Flags::Negative));
         assert!(cpu.status.get(Flags::Zero));
     }
@@ -70,23 +137,23 @@ mod test {
             get_instruction(&[TAX, 0x00]).unwrap(),
             (
                 Instruction::Trr {
-                    origin: _,
-                    destination: _,
+                    origin: Register::A,
+                    destination: Register::X,
                 },
-                1
+                0x8001
             )
         ));
 
         let mut cpu = Cpu::new();
-        cpu.register_a = 0xf0;
-        cpu.load_and_run(vec![TAX, 0x00]).unwrap();
+        cpu.load_and_run_test(vec![LDA_IMMEDIATE, 0xf0, TAX, 0x00])
+            .unwrap();
         assert_eq!(cpu.register_x, 0xf0);
         assert!(cpu.status.get(Flags::Negative));
         assert!(!cpu.status.get(Flags::Zero));
 
         let mut cpu = Cpu::new();
-        cpu.register_x = 0x50;
-        cpu.load_and_run(vec![TAX, 0x00]).unwrap();
+        cpu.load_and_run_test(vec![LDX_IMMEDIATE, 0x50, TAX, 0x00])
+            .unwrap();
         assert_eq!(cpu.register_x, 0x00);
         assert!(!cpu.status.get(Flags::Negative));
         assert!(cpu.status.get(Flags::Zero));
@@ -96,19 +163,23 @@ mod test {
     fn inx() {
         assert!(matches!(
             get_instruction(&[INX, 0x00]).unwrap(),
-            (Instruction::In { destination: _ }, 1)
+            (
+                Instruction::In {
+                    destination: Register::X
+                },
+                0x8001
+            )
         ));
 
         let mut cpu = Cpu::new();
-        cpu.register_x = 0xf0;
-        cpu.load_and_run(vec![INX, 0x00]).unwrap();
-        assert_eq!(cpu.register_x, 0xf1);
+        cpu.load_and_run_test(vec![LDX_IMMEDIATE, 0xf1, INX, 0x00])
+            .unwrap();
         assert!(cpu.status.get(Flags::Negative));
         assert!(!cpu.status.get(Flags::Zero));
 
         let mut cpu = Cpu::new();
-        cpu.register_x = 0xff;
-        cpu.load_and_run(vec![INX, 0x00]).unwrap();
+        cpu.load_and_run_test(vec![LDX_IMMEDIATE, 0xff, INX, 0x00])
+            .unwrap();
         assert_eq!(cpu.register_x, 0x00);
         assert!(!cpu.status.get(Flags::Negative));
         assert!(cpu.status.get(Flags::Zero));
@@ -125,41 +196,47 @@ use self::{memory::Memory, status::Status};
 pub struct Cpu {
     pub register_a: u8,
     pub register_x: u8,
+    pub register_y: u8,
+    pub stack_pointer: u8,
     pub status: Status,
     pub program_counter: u16,
     memory: Memory,
 }
 
-type RegisterMutable = fn(&mut Cpu) -> &mut u8;
-type RegisterImmutable = fn(&Cpu) -> u8;
+#[derive(Debug)]
+pub enum Register {
+    X,
+    Y,
+    A,
+}
 
 impl Cpu {
-    // new, getters
-
     pub fn new() -> Self {
         Cpu {
             register_a: 0x0,
             register_x: 0x0,
+            register_y: 0x0,
+            stack_pointer: 0xff,
             status: Status::new(),
             program_counter: 0x0,
             memory: Memory::new(),
         }
     }
 
-    pub fn register_a_mut(&mut self) -> &mut u8 {
-        &mut self.register_a
+    pub fn set_register(&mut self, destination: &Register, value: u8) {
+        match destination {
+            Register::X => self.register_x = value,
+            Register::Y => self.register_y = value,
+            Register::A => self.register_a = value,
+        }
     }
 
-    pub fn register_a(&self) -> u8 {
-        self.register_a
-    }
-
-    pub fn register_x_mut(&mut self) -> &mut u8 {
-        &mut self.register_x
-    }
-
-    pub fn register_x(&self) -> u8 {
-        self.register_x
+    pub fn get_register(&self, origin: &Register) -> u8 {
+        match origin {
+            Register::X => self.register_x,
+            Register::Y => self.register_y,
+            Register::A => self.register_a,
+        }
     }
 }
 
@@ -169,23 +246,48 @@ impl Cpu {
         self.status.set(Flags::Negative, (register_value as i8) < 0);
     }
 
+    pub fn reset(&mut self) -> color_eyre::Result<()> {
+        self.register_a = 0;
+        self.register_x = 0;
+        self.register_y = 0;
+        self.stack_pointer = 0xff;
+        self.status = Status::new();
+
+        self.program_counter = self.memory.read_u16(0xFFFC)?;
+        Ok(())
+    }
+
     pub fn load_and_run(&mut self, program: Vec<u8>) -> color_eyre::Result<()> {
-        self.load(program);
+        self.load(program)?;
+        self.reset()?;
         self.run()?;
 
         Ok(())
     }
 
-    pub fn load(&mut self, program: Vec<u8>) {
-        self.memory.load(0x8000, &program);
+    #[cfg(test)]
+    pub fn load_and_run_test(&mut self, program: Vec<u8>) -> color_eyre::Result<()> {
+        self.load(program)?;
+        self.reset()?;
         self.program_counter = 0x8000;
+        self.run()?;
+
+        Ok(())
     }
 
+    pub fn load(&mut self, program: Vec<u8>) -> color_eyre::eyre::Result<()> {
+        self.memory.load(0x8000, &program)?;
+
+        Ok(())
+    }
+}
+
+impl Cpu {
     pub fn run(&mut self) -> color_eyre::Result<()> {
         loop {
-            let (instruction, offset) =
+            let (instruction, program_counter) =
                 Instruction::get_instruction(&self.memory, &self.program_counter)?;
-            self.program_counter += offset;
+            self.program_counter = program_counter;
 
             use Instruction::*;
 
@@ -195,27 +297,31 @@ impl Cpu {
                     destination,
                     ld_type,
                 } => {
-                    match ld_type {
-                        LdType::Immediate { immediate } => {
-                            *destination(self) = immediate;
+                    let value: u8 = match ld_type {
+                        LdType::Immediate { immediate } => immediate,
+                        LdType::ZeroPage { address } => self.memory.read(address as u16)?,
+                        LdType::ZeroPageX { address } => {
+                            self.memory.read((address + self.register_x) as u16)?
                         }
-                    }
-                    let register_value = *destination(self);
-                    self.set_zero_and_negative(register_value);
+                        LdType::Absolute { address } => self.memory.read(address)?,
+                    };
+                    self.set_register(&destination, value);
+                    self.set_zero_and_negative(value);
                 }
                 Trr {
                     origin,
                     destination,
                 } => {
-                    *destination(self) = origin(self);
-                    let register_value = *destination(self);
-                    self.set_zero_and_negative(register_value)
+                    let value = self.get_register(&origin);
+                    self.set_register(&destination, value);
+                    self.set_zero_and_negative(value);
                 }
                 In { destination } => {
-                    let (result, _overflow) = u8::overflowing_add(*destination(self), 1);
-                    *destination(self) = result;
-                    let register_value: u8 = *destination(self);
-                    self.set_zero_and_negative(register_value);
+                    let value = self.get_register(&destination);
+                    let (value, overflow) = value.overflowing_add(1);
+                    self.set_register(&destination, value);
+                    self.status.set(Flags::Overflow, overflow);
+                    self.set_zero_and_negative(value);
                 }
             }
         }
